9452e6f1da4f818f05ae97a59e0975ef7d35ce92
mahaawadh1 <mahaaaq897@gmail.com>
2024-04-16 14:06:35 +0300

    maha

---
+++ WebApplication2/Controllers/EmployeeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            List<Employee> employees = new List<Employee>()
+        {
+            new Employee { FirstName = "maha", LastName = "salem",Email=" MAHA@gmail.com" , Department = "it"},
+            new Employee { FirstName = "Amar", LastName = "alfailkawi",Email="amar@gmail.com" , Department = "it"},
+             new Employee { FirstName = "haya", LastName = "alqahtani",Email="haya@gmz" , Department = "it"}
+        };
+          
+            return View(employees);
+        }
+    }
+}
---
+++ WebApplication2/Models/Employee.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApplication2.Models
+{
+    public class Employee
+    {
+       
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string Email { get; set; }
+            public string Department { get; set; }
+        }
+    }
+
